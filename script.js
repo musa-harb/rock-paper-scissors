00e8cc736b5568b54c9cb853cdca8ed03fcb23ac
musa-harb <musa.harb@gmail.com>
2022-12-31 09:10:50 -0800

    Add getPlayerChoice() function to get the player's selection

--- script.js
+++ script.js
@@ -3,10 +3,23 @@
 
 //getComputerChoice returns rock, paper,or scissors as a string
 //based on a randomly generated number in 0 to 2 range.
-function getComputerChoice(){
+function getComputerChoice() {
     const randomNum = Math.floor(Math.random() * 3);
     const choices = ["rock", "paper", "scissors"];
     return choices[randomNum];
 }
 
+//getPlayerChoice prompt the user to enter one of the three choices
+//rock, paper, or scissors and returns the user's choice. If the 
+//user enters a different string other than the 3 choices, it prompt
+//the user again until the user enters one of the 3 valid choices 
+function getPlayerChoice() {
+    let userChoice = '';
+    while (userChoice !== 'paper' && userChoice !== 'rock' && userChoice !== 'scissors') {        
+        userChoice = (prompt("Please make a choice, Rock, Paper, or Scissors")).toLowerCase();
+    }
+    return userChoice;
+}
+
 console.log(getComputerChoice());
+console.log(getPlayerChoice());
