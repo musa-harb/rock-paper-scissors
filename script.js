1828929cf12fb2bc6865689a3797f7d9f35c16e1
musa-harb <musa.harb@gmail.com>
2022-12-31 08:37:26 -0800

    Add getComputerChoice() function to get the computer choice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+
+//getComputerChoice returns rock, paper,or scissors as a string
+//based on a randomly generated number in 0 to 2 range.
+function getComputerChoice(){
+    const randomNum = Math.floor(Math.random() * 3);
+    const choices = ["rock", "paper", "scissors"];
+    return choices[randomNum];
+}
+
+console.log(getComputerChoice());
