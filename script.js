849874398ccbafef229f9771c5e0437b84af893a
musa-harb <musa.harb@gmail.com>
2022-12-31 09:50:10 -0800

    Add playRound() function to play round based on player and computer selections

--- script.js
+++ script.js
@@ -15,11 +15,40 @@ function getComputerChoice() {
 //the user again until the user enters one of the 3 valid choices 
 function getPlayerChoice() {
     let userChoice = '';
-    while (userChoice !== 'paper' && userChoice !== 'rock' && userChoice !== 'scissors') {        
+    while (userChoice !== 'paper' && userChoice !== 'rock' && userChoice !== 'scissors') {
         userChoice = (prompt("Please make a choice, Rock, Paper, or Scissors")).toLowerCase();
     }
     return userChoice;
 }
 
-console.log(getComputerChoice());
-console.log(getPlayerChoice());
+//playRound() function takes the playerSelection and computerSelection
+//and returns a string that indicates the winner based on the value of
+//the playerSelection and computerSelection.
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return `You Win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats `+
+        `${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
+
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return `You Win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats `+
+        `${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
+
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return `You Win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats `+
+        `${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`;
+    } else if (playerSelection === computerSelection) {
+        return "It's a tie!"
+    } else {
+        return `You Lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats `+
+        `${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`;
+    }
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+console.log(playerSelection);
+console.log(computerSelection);
+
+console.log(playRound(playerSelection, computerSelection));
